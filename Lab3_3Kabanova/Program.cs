bc4e966daaafc2dd8f8ea8036f4b0bf2aaad5378
alexa <alexa@DESKTOP-RV4BEBB>
2021-11-11 13:55:20 +0200

    Добавьте файлы проекта.

---
+++ Lab3_3Kabanova/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Kabanova3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //3.3 Вариант 6
+            try
+            {
+                Console.WriteLine("Введите n:");
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите x:");
+                double x = double.Parse(Console.ReadLine());
+                double S = 0;
+                for (int i = 1; i <= n; i++)
+                {
+                    long F = 1;
+                    for (int j = 1; j < i - 1; j++) F *= j;
+                    S += Math.Cos((i - 1) * (Math.PI / 4)) / (i - 1) * F;
+                }
+                Console.WriteLine("S={0:F2}", S);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
